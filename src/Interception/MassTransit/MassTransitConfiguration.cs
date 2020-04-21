ab78e3a8c13a9b6504a234d72efc1e7c213f1f14
Andrey Baboshin <andrey.baboshin@gmail.com>
2020-04-22 00:14:55 +0200

    masstransit configuration

---
+++ src/Interception/MassTransit/MassTransitConfiguration.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Interception.MassTransit
+{
+    /// <summary>
+    /// Configuration for masstransit
+    /// </summary>
+    public class MassTransitConfiguration
+    {
+        /// <summary>
+        /// Section name in configuration
+        /// </summary>
+        public static readonly string SectionKey = "masstransit_metrics";
+
+        /// <summary>
+        /// enable publisher tracking
+        /// </summary>
+        public bool PublisherEnabled { get; set; } = true;
+
+        /// <summary>
+        /// publisher metric name
+        /// </summary>
+        public string PublisherName { get; set; } = "masstransit_publish";
+
+        /// <summary>
+        /// enable consumer tracking
+        /// </summary>
+        public bool ConsumerEnabled { get; set; } = true;
+
+        /// <summary>
+        /// consumer metric name
+        /// </summary>
+        public string ConsumerName { get; set; } = "masstransit_consumer";
+    }
+}
