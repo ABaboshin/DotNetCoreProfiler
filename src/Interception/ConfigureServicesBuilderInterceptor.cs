62b94b4900510e666e59496638a0b64420910d09
Andrey Baboshin <ABaboshin@users.noreply.github.com>
2020-04-23 18:19:32 +0200

    inject logger (#11)

--- src/Interception/ConfigureServicesBuilderInterceptor.cs
+++ src/Interception/ConfigureServicesBuilderInterceptor.cs
@@ -1,10 +1,19 @@
 ﻿using Interception.Common;
 using Interception.Observers;
-using Interception.Observers.Configuration;
+using Interception.OpenTracing.Prometheus;
+using Interception.Serilog;
 using Interception.StackExchangeRedis;
+using Interception.Tracing;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using OpenTracing.Util;
+using Serilog;
+using Serilog.AspNetCore;
+using Serilog.Core;
+using Serilog.Events;
+using Serilog.Formatting.Json;
 using System;
-using System.Diagnostics;
 
 namespace Interception
 {
@@ -14,19 +23,60 @@ namespace Interception
         public static object Invoke(object builder, object instance, object services, int mdToken, long moduleVersionPtr)
         {
             Console.WriteLine($"Configure additional services {builder.GetType().Name} {instance.GetType().Name} {services.GetType().Name}");
+            
+            DiagnosticsObserver.ConfigureAndStart();
+
+            StackExchangeRedisInterception.Configure();
+
+            var loggerFactory = new SerilogLoggerFactory(CreateLogger(), false);
+            ((IServiceCollection)services).AddSingleton((ILoggerFactory)loggerFactory);
+
+            ConfigureMetrics(loggerFactory);
+
+            return MethodExecutor.ExecuteMethod(builder, new object[] { instance, services }, mdToken, moduleVersionPtr, true);
+        }
+
+        private static void ConfigureMetrics(ILoggerFactory loggerFactory)
+        {
             var configuration = new ConfigurationBuilder()
                 .AddEnvironmentVariables()
                 .Build();
 
-            var aspNetCoreConfiguration = configuration.GetSection(AspNetCoreConfiguration.SectionKey).Get<AspNetCoreConfiguration>();
-            var httpHandlerConfiguration = configuration.GetSection(HttpHandlerConfiguration.SectionKey).Get<HttpHandlerConfiguration>();
-            var entityFrameworkCoreConfiguration = configuration.GetSection(EntityFrameworkCoreConfiguration.SectionKey).Get<EntityFrameworkCoreConfiguration>();
+            var tracingConfiguration = configuration.GetSection(TracingConfiguration.SectionKey).Get<TracingConfiguration>();
 
-            DiagnosticListener.AllListeners.Subscribe(new DiagnosticsObserver(aspNetCoreConfiguration, httpHandlerConfiguration, entityFrameworkCoreConfiguration));
+            if (tracingConfiguration.Collector.ToLower() == "jaeger")
+            {
+                var config = Jaeger.Configuration.FromEnv(loggerFactory);
+                var tracer = config.GetTracer();
 
-            StackExchangeRedisInterception.Configure();
+                GlobalTracer.Register(tracer);
+            }
+            else
+            {
+                var config = PrometheusConfiguration.FromEnv(loggerFactory);
+                var tracer = config.GetTracer();
 
-            return MethodExecutor.ExecuteMethod(builder, new object[] { instance, services }, mdToken, moduleVersionPtr, true);
+                GlobalTracer.Register(tracer);
+            }
+        }
+
+        private static Logger CreateLogger()
+        {
+            var logLevel = ParseLoggingLevel(Environment.GetEnvironmentVariable("LOG_LEVEL"));
+
+            var logger = new LoggerConfiguration()
+                .Enrich.FromLogContext()
+                .Enrich.With((ILogEventEnricher)new SerilogEnricher())
+                .MinimumLevel.Is(LogEventLevel.Verbose)
+                .WriteTo.Console(new JsonFormatter(renderMessage: true), logLevel)
+                .CreateLogger();
+            return logger;
+        }
+
+        private static LogEventLevel ParseLoggingLevel(string logLevelRaw)
+        {
+            Enum.TryParse(logLevelRaw, out LogEventLevel level);
+            return level as LogEventLevel? ?? LogEventLevel.Verbose;
         }
     }
 }
