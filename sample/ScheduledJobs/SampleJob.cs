da60baa3beafb53984711aec4203deecf1e7271e
Andrey Baboshin <ABaboshin@users.noreply.github.com>
2020-05-18 23:40:58 +0200

    just for fun: intercept ILogger.LogInformation (#55)

--- sample/ScheduledJobs/SampleJob.cs
+++ sample/ScheduledJobs/SampleJob.cs
@@ -1,4 +1,5 @@
 ﻿using MassTransit;
+using Microsoft.Extensions.Logging;
 using Quartz;
 using SampleApp.MessageBus;
 using System.Threading.Tasks;
@@ -8,14 +9,17 @@ namespace SampleApp.ScheduledJobs
     public class SampleJob : IJob
     {
         private readonly IBusControl _busControl;
+        private readonly ILogger<SampleJob> _logger;
 
-        public SampleJob(IBusControl busControl)
+        public SampleJob(IBusControl busControl, ILoggerFactory loggerFactory)
         {
             _busControl = busControl;
+            _logger = loggerFactory.CreateLogger<SampleJob>();
         }
 
         public async Task Execute(IJobExecutionContext context)
         {
+            _logger.LogInformation("test");
             await _busControl.Publish(new MyMessage { Id = 3 });
         }
     }
---
+++ src/Interception/Serilog/LogInformationInterceptor.cs
@@ -0,0 +1,18 @@
+﻿using Interception.Attributes;
+using Interception.Core;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Interception.Serilog
+{
+    [StrictIntercept(CallerAssembly = "", TargetAssemblyName = "Microsoft.Extensions.Logging.Abstractions", TargetMethodName = "LogInformation", TargetTypeName = "Microsoft.Extensions.Logging.LoggerExtensions", TargetMethodParametersCount = 3)]
+    public class LogInformationInterceptor : BaseInterceptor
+    {
+        public override void ExecuteBefore()
+        {
+            Console.WriteLine($"Call LogInformationInterceptor {GetParameter(0)} {GetParameter(1)} {GetParameter(2)}");
+            base.ExecuteBefore();
+        }
+    }
+}
