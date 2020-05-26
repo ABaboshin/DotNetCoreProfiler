771f3e1d9293383d750f1ba7fd344fd88027ca95
Andrey Baboshin <andrey.baboshin@gmail.com>
2020-05-26 23:34:23 +0200

    intercept Serilog.SerilogWebHostBuilderExtensions.UseSerilog

---
+++ src/Interception/Serilog/DisableUseSerilog.cs
@@ -0,0 +1,21 @@
+﻿using Interception.Attributes;
+using Interception.Core;
+
+namespace Interception.Serilog
+{
+    [StrictIntercept(
+        TargetAssemblyName = "Serilog.AspNetCore",
+        TargetMethodName = "UseSerilog",
+        TargetTypeName = "Serilog.SerilogWebHostBuilderExtensions",
+        TargetMethodParametersCount = 3)]
+    public class DisableUseSerilog : BaseInterceptor
+    {
+        public override int Priority => 0;
+
+        public override bool SkipExecution()
+        {
+            Result = GetParameter(0);
+            return true;
+        }
+    }
+}
