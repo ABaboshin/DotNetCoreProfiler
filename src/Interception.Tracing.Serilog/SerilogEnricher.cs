4be05db0f05e3c0e41cd768bae8d701087d9f7cb
Andrey Baboshin <andrey.baboshin@gmail.com>
2020-06-15 19:25:05 +0200

    serilog

---
+++ src/Interception.Tracing.Serilog/SerilogEnricher.cs
@@ -0,0 +1,23 @@
+﻿using OpenTracing.Util;
+using Serilog.Core;
+using Serilog.Events;
+
+namespace Interception.Serilog
+{
+    public class SerilogEnricher :
+        ILogEventEnricher
+    {
+        public void Enrich(LogEvent logEvent, ILogEventPropertyFactory factory)
+        {
+            if (GlobalTracer.Instance.ActiveSpan is null)
+            {
+                return;
+            }
+
+            var traceId = GlobalTracer.Instance.ActiveSpan.Context.TraceId;
+
+            if (!string.IsNullOrEmpty(traceId))
+                logEvent.AddOrUpdateProperty(factory.CreateProperty("TraceId", traceId, true));
+        }
+    }
+}
