69c705076b4cf80ddfdb3a0f2a87180c30878a60
Andrey Baboshin <ABaboshin@users.noreply.github.com>
2020-09-25 23:06:37 +0200

    feat: dummy c++ unit tests (#78)

---
+++ tests/Profiler.Native.Tests/main.cpp
@@ -0,0 +1,6 @@
+#include "pch.h"
+
+int main(int argc, char** argv) {
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
---
+++ tests/Profiler.Native.Tests/pch.h
@@ -0,0 +1,5 @@
+#pragma once
+
+#define GTEST_LANG_CXX11 1
+
+#include "gtest/gtest.h"
