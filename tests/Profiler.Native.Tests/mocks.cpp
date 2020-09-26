684335ac11b240bab53cb5f243b3599ce3d380d4
Andrey Baboshin <ABaboshin@users.noreply.github.com>
2020-09-26 20:25:23 +0200

    test: signature parser (#81)

--- tests/Profiler.Native.Tests/mocks.cpp
+++ tests/Profiler.Native.Tests/mocks.cpp
@@ -1,6 +1,6 @@
 #include "mocks.h"
 
-static void RaiseException(DWORD dwExceptionCode,
+void RaiseException(DWORD dwExceptionCode,
 	DWORD dwExceptionFlags,
 	DWORD nNumberOfArguments,
 	CONST ULONG_PTR* lpArguments)
--- tests/Profiler.Native.Tests/mocks.h
+++ tests/Profiler.Native.Tests/mocks.h
@@ -2,7 +2,7 @@
 
 #include "cor.h"
 
-static void RaiseException(DWORD dwExceptionCode,
+void RaiseException(DWORD dwExceptionCode,
 	DWORD dwExceptionFlags,
 	DWORD nNumberOfArguments,
 	CONST ULONG_PTR* lpArguments);
