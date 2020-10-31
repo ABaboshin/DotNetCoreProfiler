05e9eaff437b1a1683b67483e77735026071ce47
Andrey Baboshin <andrey.baboshin@gmail.com>
2020-10-31 21:13:28 +0100

    integration tests: 1 interceptor

---
+++ tests/integration/1interceptor/interceptor/Class1.cs
@@ -0,0 +1,22 @@
+﻿using Interception.Attributes;
+using Interception.Core;
+using System;
+
+namespace interceptor
+{
+    [StrictIntercept(TargetAssemblyName = "app", TargetMethodName = "TestM", TargetTypeName = "app.Program", TargetMethodParametersCount = 0)]
+    public class Class1 : BaseInterceptor
+    {
+        public override int Priority => 0;
+
+        public override void ExecuteBefore()
+        {
+            Console.WriteLine("ExecuteBefore");
+        }
+
+        public override void ExecuteAfter()
+        {
+            Console.WriteLine("ExecuteAfter");
+        }
+    }
+}
