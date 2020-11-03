50452c1e673a0557a74dac522c2a1f8312627e68
Andrey Baboshin <ABaboshin@users.noreply.github.com>
2020-11-03 19:41:06 +0100

    tests: intercept this (#94)

---
+++ tests/integration/this-not-null-interceptor/interceptor/Class1.cs
@@ -0,0 +1,21 @@
+﻿using Interception.Attributes;
+using Interception.Core;
+using System;
+
+namespace interceptor
+{
+    [StrictIntercept(TargetAssemblyName = "app", TargetMethodName = "X", TargetTypeName = "app.Cls1", TargetMethodParametersCount = 0)]
+    public class Class1 : BaseInterceptor
+    {
+        public override int Priority => 1;
+
+        public override void ExecuteBefore()
+        {
+            Console.WriteLine(this->This != null);
+        }
+
+        public override void ExecuteAfter()
+        {
+        }
+    }
+}
