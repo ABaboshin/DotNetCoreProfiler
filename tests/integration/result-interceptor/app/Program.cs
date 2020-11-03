fc9959f4a7e135ba13c141f2db9dfe6a34fd874b
Andrey Baboshin <ABaboshin@users.noreply.github.com>
2020-11-03 20:42:58 +0100

    tests: result (#97)

---
+++ tests/integration/result-interceptor/app/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace app
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      TestM(1);
+    }
+
+    static int TestM(int i)
+    {
+      return 1;
+    }
+  }
+}
