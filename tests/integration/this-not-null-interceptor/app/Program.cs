a66c34edd8a9719d6d1d40e425737915cb8959f7
Andrey Baboshin <andrey.baboshin@complevo.de>
2020-11-03 19:39:42 +0100

    tests: intercept this

---
+++ tests/integration/this-not-null-interceptor/app/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace app
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      new Cls1().X();
+    }
+  }
+
+  public class Cls1 {
+    public void X() {}
+  }
+}
