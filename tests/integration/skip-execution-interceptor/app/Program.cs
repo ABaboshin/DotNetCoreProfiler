206828bfe7e828e74d1180cd1588c5b51060d3db
Andrey Baboshin <ABaboshin@users.noreply.github.com>
2020-11-03 20:33:15 +0100

    test: skip execution (#96)

---
+++ tests/integration/skip-execution-interceptor/app/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace app
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      TestM(1);
+    }
+
+    static void TestM(int i)
+    {
+      Console.WriteLine("oops");
+    }
+  }
+}
---
+++ tests/integration/skip-execution-interceptor/interceptor/Class1.cs
@@ -0,0 +1,27 @@
+﻿using Interception.Attributes;
+using Interception.Core;
+using System;
+
+namespace interceptor
+{
+    [StrictIntercept(TargetAssemblyName = "app", TargetMethodName = "TestM", TargetTypeName = "app.Program", TargetMethodParametersCount = 1)]
+    public class Class1 : BaseInterceptor
+    {
+        public override int Priority => 1;
+
+        public override bool SkipExecution()
+        {
+            return true;
+        }
+
+        public override void ExecuteBefore()
+        {
+            Console.WriteLine(GetParameter(0));
+        }
+
+        public override void ExecuteAfter()
+        {
+          Console.WriteLine(GetParameter(0));
+        }
+    }
+}
