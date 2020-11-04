215545cb1414a414558b1da31185da7124fed96d
Andrey Baboshin <andrey.baboshin@gmail.com>
2020-11-04 17:45:03 +0100

    samples dd

---
+++ samples/Readme.md
@@ -0,0 +1,12 @@
+# Samples
+
+## Deadlock Detection
+
+See [deadlock-detection](deadlock-detection).
+
+In order to run:
+```
+cd .. && yarn samples:dd
+```
+
+As a result you'll see `Interception.DeadlockDetection.DeadlockException: Monitor o1->Thread  4; Thread  5->Thread  4; Monitor o2->Thread  4` in the output.
---
+++ samples/deadlock-detection/app/Program.cs
@@ -0,0 +1,47 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+
+namespace app
+{
+  class Program
+  {
+    public static object o1 = "o1";
+    public static object o2 = "o2";
+
+    public static void T1()
+    {
+      lock (o1)
+      {
+        Thread.Sleep(1000);
+        lock (o2)
+        {
+
+        }
+      }
+    }
+
+    public static void T2()
+    {
+      lock (o2)
+      {
+        Thread.Sleep(1000);
+        lock (o1)
+        {
+
+        }
+      }
+    }
+
+    static void Main(string[] args)
+    {
+      var t1 = new Thread(T1);
+      var t2 = new Thread(T2);
+
+      t1.Start();
+      t2.Start();
+
+      t1.Join();
+      t2.Join();
+    }
+  }
+}
