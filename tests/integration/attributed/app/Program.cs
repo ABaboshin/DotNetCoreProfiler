8e7e478f1e35f8e821325b163e9ad4d8dff36b21
Andrey Baboshin <andrey.baboshin@gmail.com>
2020-11-29 19:25:21 +0100

    tests attributed based

---
+++ tests/integration/attributed/app/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Interception.Attributes.Validation;
+
+namespace app
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      TestM(3);
+    }
+
+    [SampleAttributes.MySampleAttribute(P1 = 6000, P2 = new[] { "an", "array" })]
+    static void TestM(int i)
+    {
+    }
+  }
+}
