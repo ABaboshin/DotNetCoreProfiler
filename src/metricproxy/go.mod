54710a71dff0d818116be5cdf84d30ae59b0f1b6
Andrey Baboshin <andrey.baboshin@gmail.com>
2020-12-30 00:31:59 +0100

    split consuming/processing

--- src/metricproxy/go.mod
+++ src/metricproxy/go.mod
@@ -4,5 +4,6 @@ go 1.14
 
 require (
 	github.com/golang/protobuf v1.4.3
+	github.com/scryner/lfreequeue v0.0.0-20121212074822-473f33702129
 	google.golang.org/protobuf v1.23.0
 )
--- src/metricproxy/main.go
+++ src/metricproxy/main.go
@@ -8,39 +8,57 @@ import (
 	"os"
 
 	"github.com/golang/protobuf/proto"
+	lfreequeue "github.com/scryner/lfreequeue"
 )
 
 const SockAddr = "/var/tracing-proxy.sock"
 
 func main() {
-	if err := os.RemoveAll(SockAddr); err != nil {
-		log.Fatal(err)
-	}
 
-	l, err := net.ListenUnix("unix", &net.UnixAddr{SockAddr, "unix"})
-	if err != nil {
-		log.Fatal("listen error:", err)
-	}
-	defer l.Close()
+	q := lfreequeue.NewQueue()
 
-	log.Printf("Listen [%s] [%s]", SockAddr, "unix")
+	watchIterator := q.NewWatchIterator()
+	iter := watchIterator.Iter()
+	defer watchIterator.Close()
+
+	go func() {
+		if err := os.RemoveAll(SockAddr); err != nil {
+			log.Fatal(err)
+		}
+
+		l, err := net.ListenUnix("unix", &net.UnixAddr{SockAddr, "unix"})
+		if err != nil {
+			log.Fatal("listen error:", err)
+		}
+		defer l.Close()
+
+		log.Printf("Listen [%s] [%s]", SockAddr, "unix")
+
+		for {
+			conn, err := l.AcceptUnix()
+			if err != nil {
+				panic(err)
+			}
+
+			var buf bytes.Buffer
+			io.Copy(&buf, conn)
+			message := &TraceMetric{}
+			err = proto.Unmarshal(buf.Bytes(), message)
+			if err != nil {
+				return
+			}
+
+			q.Enqueue(*message)
+
+			conn.Close()
+		}
+	}()
 
 	for {
-		conn, err := l.AcceptUnix()
-		if err != nil {
-			panic(err)
+		select {
+		case v := <-iter:
+			val := v.(TraceMetric)
+			log.Printf("Got type [%s] value [%f] startDate [%f] finishDate [%f]", *val.Type, *val.Value, *val.StartDate, *val.FinishDate)
 		}
-
-		var buf bytes.Buffer
-		io.Copy(&buf, conn)
-		message := &TraceMetric{}
-		err = proto.Unmarshal(buf.Bytes(), message)
-		if err != nil {
-			return
-		}
-
-		log.Printf("Got type [%s] value [%f] startDate [%f] finishDate [%f]", *(*message).Type, *(*message).Value, *(*message).StartDate, *(*message).FinishDate)
-
-		conn.Close()
 	}
 }
